426eebb43a651f99ef5eccfb5c9b71ef385853ea
jensjeflensje <30746389+jensjeflensje@users.noreply.github.com>
2018-05-17 13:23:33 +0200

    Add files via upload

--- bot/index.py
+++ bot/index.py
@@ -1,1 +1,43 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
 
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+@asyncio.coroutine
+def on_ready():
+    print("Bot is ready!")
+
+@client.event
+@asyncio.coroutine
+async def on_message(message):
+    if message.content == "cookie":
+        await client.send_message(message.channel, ":cookie:")
+    if message.content.lower().startswith("!test"):
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s> Dit is een test, " % (userID))
+    if message.content.lower().startswith("!say"):
+        if message.author.id == "283554212019699722":
+            args = message.content.split(" ")
+            await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+        else:
+            await client.send_message(message.channel, "Je hebt hier geen permissies voor!")
+    if message.content.lower().startswith("!speel"):
+        if message.author.id == "283554212019699722":
+            args = message.content.split(" ")
+            await client.send_message(message.channel, " ".join(args[1:]))
+            await client.change_presence(game=discord.Game(name=" ".join(args[1:])))
+        else:
+            await client.send_message(message.channel, "Je hebt hier geen permissies voor!")
+    if message.content.lower().startswith("!create"):
+        userID = message.author.id
+        server = message.server
+        args = message.content.split(" ")
+        await client.create_channel(server, (" ".join(args[1:]).replace(" ", "-")), type=discord.ChannelType.text)
+        await client.send_message(message.channel, "Je hebt " % (" ".join(args[1:])) % "gemaakt!")
+
+client.run("NDExNTQ2MDgzODQwMjI5Mzg3.DaFJ5w.5wVQUDbv68RxU4k21i1gUXy8Ezc")
--- main.py
+++ main.py
@@ -24,13 +24,17 @@ def dump_yaml(data, file):
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ["py"]
+           filename.rsplit('.', 1)[1].lower() in ["py", "js"]
 
 @app.route("/")
 def index():
     try:
-        if session["user"] == True:
-            return render_template("index.html")
+        username = session["user"]
+    except:
+        return redirect(url_for("login"))
+    try:
+        if session["logged"] == True:
+            return render_template("index.html", username=username)
         else:
             return redirect(url_for("login"))
     except:
@@ -45,7 +49,8 @@ def login():
         if load_yaml("data/accounts.yml").get(username) != None:
             if request.form["password"] == load_yaml("data/accounts.yml").get(username):
                 print(load_yaml("data/accounts.yml").get(username))
-                session["user"] = True
+                session["logged"] = True
+                session["user"] = username
                 return "<meta http-equiv='refresh' content='0; url=/' />"
             else:
                 return render_template("login.html")
@@ -54,34 +59,45 @@ def login():
     else:
         return render_template("login.html")
 
-@app.route("/start", methods=["POST"])
-def start():
-    subprocess.call("pkill -f index.py", shell=True)
-    subprocess.Popen("python3 bot/index.py", shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+@app.route("/start/<user>/py", methods=["POST"])
+def startpy(user):
+    subprocess.call("pkill -f {}.index.py".format(user), shell=True)
+    subprocess.Popen("python3 bot/{}.index.py".format(user), shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+    print(user + "heeft zijn python bot aangezet")
     return "<meta http-equiv='refresh' content='0; url=/' />"
 
 
-@app.route("/stop", methods=["POST"])
-def stop():
-    subprocess.call("pkill -f index.py", shell=True)
+@app.route("/stop/<user>/py", methods=["POST"])
+def stop(user):
+    subprocess.call("pkill -f {}.index.py".format(user), shell=True)
+    print(user + "heeft zijn python bot uitgezet")
     return "<meta http-equiv='refresh' content='0; url=/' />"
 
-@app.route("/upload", methods=["POST"])
-def upload():
+
+@app.route("/start/<user>/js", methods=["POST"])
+def startjs(user):
+    subprocess.call("pkill -9 -f {}.index.js".format(user), shell=True)
+    subprocess.Popen("node bot/{}.index.js".format(user), shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+    print(user + "heeft zijn javascript bot aangezet")
+    return "<meta http-equiv='refresh' content='0; url=/' />"
+
+@app.route("/stop/<user>/js", methods=["POST"])
+def stopjs(user):
+    subprocess.call("pkill -9 -f {}.index.js".format(user), shell=True)
+    print(user + "heeft zijn javascript bot uitgezet")
+    return "<meta http-equiv='refresh' content='0; url=/' />"
+
+@app.route("/upload/<user>", methods=["POST"])
+def upload(user):
     file = request.files['file']
     print(file)
     if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
+        filename = secure_filename(user + "." + file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         return "<meta http-equiv='refresh' content='0; url=/' />"
     else:
         return "<meta http-equiv='refresh' content='0; url=/' />"
 
-
-@app.route("/profiel")
-@app.route("/profiel/<naam>")
-def profiel(naam=None):
-    return render_template("profiel.html", naam=naam)
 
 if __name__ == "__main__":
     app.run(debug=True, port=10000, host="0.0.0.0")
